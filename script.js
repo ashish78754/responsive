1424841ca35abea2671a13da58075dbc9fc3c38c
Ashish Kumar <155081588+ashish78754@users.noreply.github.com>
2024-11-10 18:44:33 +0530

    Update script.js
    responsive changes for min 600px width

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
-var tl= gsap.timeline()
+var tl = gsap.timeline();
 
-tl.from("#logo",{
-    y:-30,
-    opacity:0,
-    duration:1,
-    delay:0.1  
-})
+if (window.matchMedia("(max-width: 600px)").matches) {
+    tl.from("#logo", {
+        y: -10,
+        opacity: 0,
+        duration: 0.8,
+        delay: 0.1
+    });
 
-tl.from("ul",{
-    y:-30,
-    opacity:0,
-    duration:1,
-    stagger:0.2
-})
+    tl.from("ul", {
+        opacity: 0,
+        duration: 0.8,
+        stagger: 0.2
+    });
 
-tl.from("ul li",{
-    y:-30,
-    opacity:0,
-    duration:1,
-    stagger:0.5
-})
+    tl.from("main h1", {
+        y: 10,
+        opacity: 0,
+        duration: 0.8,
+        scale: 0.2
+    });
 
-tl.from("main h1",{
-    y:20,
-    opacity:0,
-    duration:1,
-    scale:0.2
-})
+    tl.from(".boxes", {
+        y: -30,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.1
+    });
 
-tl.from(".boxes",{
-    y:-30,
-    opacity:0,
-    duration:1,
-    stagger:0.1
-})
+} else {
+    tl.from("#logo", {
+        y: -30,
+        opacity: 0,
+        duration: 1,
+        delay: 0.1
+    });
+
+    tl.from("ul", {
+        y: -30,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.2
+    });
+
+    tl.from("ul li", {
+        y: -30,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.5
+    });
+
+    tl.from("main h1", {
+        y: 20,
+        opacity: 0,
+        duration: 1,
+        scale: 0.2
+    });
+
+    tl.from(".boxes", {
+        y: -30,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.1
+    });
+}
+
